a9ca261f1893feeb9d7cd2f20c6eb07917bb3cf4
Unknown <p.tomlinson@mdx.ac.uk>
2018-01-12 13:07:52 +0000

    Basic First Person and Door
    Added standard assets
    Added FPS controller to level1
    Added out of scale sliding door and basic script

---
+++ WalkingSimBase/Assets/PT_scripts/SceneSwitchOnButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchOnButton : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space"))
+		{
+			print("space key was pressed");
+			// Load the next scene number 1 (this is number 0 scene)
+			SceneManager.LoadScene(1);
+		}
+	}
+}
