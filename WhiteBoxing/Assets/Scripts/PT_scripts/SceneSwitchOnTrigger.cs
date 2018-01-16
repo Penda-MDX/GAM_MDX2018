9049d6e4012a5f01f86de10e2e9db5289f3d972d
Unknown <p.tomlinson@mdx.ac.uk>
2018-01-16 17:17:12 +0000

    Level Design Blocking Out
    WhiteBox level layout

---
+++ WhiteBoxing/Assets/Scripts/PT_scripts/SceneSwitchOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchOnTrigger : MonoBehaviour {
+
+	//3D trigger collider activated when RigidBody 
+	//or Character Controller enters the collider
+	void OnTriggerEnter (Collider other) {
+		print ("Trigger box entered");
+		SceneManager.LoadScene (0);
+	}
+
+}
---
+++ WhiteBoxing/Assets/Scripts/PT_scripts/doorButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorButtonScript : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("e"))
+		{
+			if (anim.GetBool ("open")) {
+				print ("Close the door");
+				anim.SetBool ("open", false);
+			} else {
+				print ("Open the door");
+				anim.SetBool ("open", true);
+			}
+		}
+	}
+}
