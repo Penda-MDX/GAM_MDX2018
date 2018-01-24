b51744eb8e02a4760e9e92e3ecac8db196fc1c70
Unknown <p.tomlinson@mdx.ac.uk>
2018-01-24 17:40:37 +0000

    Added Basic and Advanced PickUp
    Invisible Hand - Pickup with and without physics

---
+++ WalkingSimBase/Assets/PT_scripts/PT_PickUpWithDistanceCheck.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PT_PickUpWithDistanceCheck : MonoBehaviour {
+
+
+    [SerializeField] private float dropDistance = 3.0f;
+    [SerializeField] private bool turnOffPhysics = false;
+    [SerializeField] private GameObject indicatorCircle;
+
+    [SerializeField] private Material availablePickUpColour;
+    [SerializeField] private Material isCarryingColour;
+
+    private bool canPickUp = false;
+    private GameObject movableObject;
+    private GameObject carriedObject;
+    
+    private bool carryingObject = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("e"))
+        {
+            if (carryingObject)
+            {
+                DropCurrent();
+            }
+            else
+            {
+                PickUpCurrent();
+            }
+        }
+
+        if (carryingObject)
+        {
+            if (Vector3.Distance(gameObject.transform.position, carriedObject.transform.position) > dropDistance)
+            {
+                DropCurrent();
+            }
+        }
+    }
+
+
+    public void PickUpCurrent()
+    {
+        if (canPickUp)
+        {
+            indicatorCircle.GetComponent<Renderer>().material = isCarryingColour;
+            carryingObject = true;
+            movableObject.transform.SetParent(gameObject.transform);
+            carriedObject = movableObject;
+
+            if (turnOffPhysics)
+            {
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+
+    public void DropCurrent()
+    {
+        carryingObject = false;
+        carriedObject.transform.SetParent(null);
+        indicatorCircle.GetComponent<Renderer>().material = availablePickUpColour;
+
+        if (turnOffPhysics)
+        {
+            carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Movable")
+        {
+            canPickUp = true;
+            movableObject = other.gameObject;
+            indicatorCircle.SetActive(true);
+            indicatorCircle.GetComponent<Renderer>().material = availablePickUpColour;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Movable")
+        {
+            canPickUp = false;
+            movableObject = null;
+            indicatorCircle.SetActive(false);
+        }
+    }
+}
