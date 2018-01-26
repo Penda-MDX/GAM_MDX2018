614441aab4d7c6e5e2af6ab7d4c3a88ac658d35f
Unknown <p.tomlinson@mdx.ac.uk>
2018-01-26 17:18:59 +0000

    Dragging, Pushing, and Picking Up
    Using physics and springs to make pulling and pushing work with first person camera.

--- WalkingSimBase/Assets/PT_scripts/PT_Drag.cs
+++ WalkingSimBase/Assets/PT_scripts/PT_Drag.cs
@@ -2,28 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PT_PickUpWithDistanceCheck : MonoBehaviour {
-
+public class PT_Drag : MonoBehaviour {
 
     [SerializeField] private float dropDistance = 3.0f;
-    [SerializeField] private bool turnOffPhysics = false;
     [SerializeField] private GameObject indicatorCircle;
-    [SerializeField] private bool lockToFront = false;
-    [SerializeField] private Vector3 lockLocalPosition = new Vector3(0, 0.5f, 0.5f);
-
+    
     [SerializeField] private Material availablePickUpColour;
     [SerializeField] private Material isCarryingColour;
 
     private bool canPickUp = false;
     private GameObject movableObject;
     private GameObject carriedObject;
-    private Rigidbody carriedRB; 
+    private Rigidbody carriedRB;
+    private SpringJoint pullSpringJoint;
+
     private bool carryingObject = false;
     private float oldDrag;
     // Use this for initialization
     void Start()
     {
-
+        pullSpringJoint = GetComponent<SpringJoint>();
     }
 
     // Update is called once per frame
@@ -58,28 +56,11 @@ public class PT_PickUpWithDistanceCheck : MonoBehaviour {
         {
             indicatorCircle.GetComponent<Renderer>().material = isCarryingColour;
             carryingObject = true;
-            movableObject.transform.SetParent(gameObject.transform);
             carriedObject = movableObject;
             carriedRB = carriedObject.GetComponent<Rigidbody>();
             oldDrag = carriedRB.drag;
             carriedRB.drag = 0;
-
-            if (lockToFront)
-            {
-                carriedRB.isKinematic = true;
-                carriedRB.useGravity = false;
-                carriedObject.transform.localPosition = lockLocalPosition;
-
-            }
-            else
-            {
-                if (turnOffPhysics)
-                {
-                    
-                    carriedRB.isKinematic = true;
-
-                }
-            }
+            pullSpringJoint.connectedBody = carriedRB;
 
         }
     }
@@ -90,17 +71,7 @@ public class PT_PickUpWithDistanceCheck : MonoBehaviour {
         carriedObject.transform.SetParent(null);
         indicatorCircle.GetComponent<Renderer>().material = availablePickUpColour;
         carriedRB.drag = oldDrag;
-
-        if (turnOffPhysics)
-        {
-            carriedRB.isKinematic = false;
-        }
-
-        if (lockToFront)
-        {
-            carriedRB.isKinematic = false;
-            carriedRB.useGravity = true;
-        }
+        pullSpringJoint.connectedBody = null;
 
     }
 
