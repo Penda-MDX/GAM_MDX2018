bee6535c596bdffd3919b7e0e15f3627ed48cf45
Unknown <p.tomlinson@mdx.ac.uk>
2019-02-06 15:53:29 +0000

    Camera Experiment pt 1
    Added time and date experiment to overlay - l key activates

---
+++ WalkingSimBase/Assets/PT_scripts/Camera/PathNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Node00", menuName = "Path Node", order = 2)]
+public class PathNode : ScriptableObject {
+
+    public GameObject startTrigger;
+    public GameObject endTrigger;
+    public GameObject[] panTrigger;
+    public Vector3[] waypoints;
+
+    public Camera currentCamera;
+
+	// Use this for initialization
+	void OnEnable () {
+		
+	}
+
+}
--- WalkingSimBase/Assets/PT_scripts/PT_LevelManager.cs
+++ WalkingSimBase/Assets/PT_scripts/PT_LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,11 +16,35 @@ public class PT_LevelManager : MonoBehaviour {
     public Text statusText;
     public float timeOnScreen = 2.5f;
 
+    public string LevelName;
+    public bool includetimerCount;
+
     private float timeComplete = 0;
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    private bool timerRunning;
+    private float timerStart;
+    private int timerHours;
+    private int timerMinutes;
+    private int timerSeconds;
+
+    private string date;
+    private string statusMessage;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        timerHours = 0;
+        timerMinutes = 0;
+        timerSeconds = 0;
+        timerRunning = false;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,7 +55,22 @@ public class PT_LevelManager : MonoBehaviour {
                 messageBox.gameObject.SetActive(false);
             }
         }
-	}
+        //timer test with key press
+        if (Input.GetKeyUp("l"))
+        {
+            //print("l pressed");
+            if (timerRunning)
+            {
+                EndTimer();
+            }
+            else
+            {
+                StartTimer();
+            }
+        }
+        ComposeStatus();
+
+    }
 
     public void ShowMessage(string currentMessage)
     {
@@ -40,5 +80,48 @@ public class PT_LevelManager : MonoBehaviour {
             messageBox.gameObject.SetActive(true);
             timeComplete = Time.time + timeOnScreen;
         }
+    }
+
+
+    public void ComposeStatus()
+    {
+        var culture = new CultureInfo("en-GB");
+        date = System.DateTime.Now.ToString(culture);
+        float timeSoFar = 0; 
+
+        if (timerRunning)
+        {
+            timeSoFar = Time.time - timerStart;
+            //print("Timer " + timeSoFar);
+            timerHours = Mathf.FloorToInt(timeSoFar/3600f);
+            timerMinutes = Mathf.FloorToInt((timeSoFar%3600f)/60);
+            timerSeconds = Mathf.FloorToInt((timeSoFar % 3600f) % 60);
+        }
+        string timer = timerHours.ToString().PadLeft(2,'0') + ":" + timerMinutes.ToString().PadLeft(2, '0') + ":" + timerSeconds.ToString().PadLeft(2, '0') + " ";
+        string tween = "";
+        if (includetimerCount)
+        {
+            statusText.text = timer.PadRight(30) + timeSoFar.ToString().PadRight(35) + LevelName.PadRight(60) + date;
+        }
+        else
+        {
+            statusText.text = timer.PadRight(30) + tween.ToString().PadRight(35) + LevelName.PadRight(60) + date;
+        }
+        
+
+    }
+
+    public void StartTimer()
+    {
+        timerStart = Time.time;
+        timerRunning = true;
+    }
+
+    public void EndTimer()
+    {
+        timerHours = 0;
+        timerMinutes = 0;
+        timerSeconds = 0;
+        timerRunning = false;
     }
 }
